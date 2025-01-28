e22349f7d9ad529849d94dac0c70b4059d46995a
tim <tjhickey@brandeis.edu>
2025-01-28 14:11:49 -0500

    sorted questions on exam by shortname

--- app.js
+++ app.js
@@ -2275,7 +2275,13 @@ const generateTex = (problems) => {
           */
           const psetId = req.params.psetId;
           const pset = await ProblemSet.findOne({_id: psetId});
-          const problems = await Problem.find({psetId: psetId}).populate('skills');
+          let problems
+             = await Problem
+                    .find({psetId: psetId})
+                    .populate('skills');
+          problems.sort((a,b) => compareSkills(a.skills[0],b.skills[0]));
+       
+         
       
   
       
